aa16cbe717590c22f8c79c6559853d8b6a0037f2
thisissandip <69596988+thisissandip@users.noreply.github.com>
2020-10-12 12:58:45 +0530

    fetch changes

--- src/apifetches.js
+++ src/apifetches.js
@@ -1,9 +1,9 @@
-const apiKey = '0b284e589d747768a9d9d72a1dd2b38c';
+const apiKEY = '0b284e589d747768a9d9d72a1dd2b38c';
 
 const endpoints = {
 	baseURL: `https://api.themoviedb.org/3/`,
-	Multi_first_half: `search/multi?api_key=${apiKey}&language=en-US`,
-	getSimilar: `similar?api_key=${apiKey}`,
+	Multi_first_half: `search/multi?api_key=${apiKEY}&language=en-US`,
+	getSimilar: `similar?api_key=${apiKEY}`,
 };
 
 export default endpoints;
