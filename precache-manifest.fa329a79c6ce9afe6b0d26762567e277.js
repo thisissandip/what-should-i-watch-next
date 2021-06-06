59c34ad4b6f0150634515903b5eccdbadad6f0a2
thisissandip <thisissandipmondal@gmail.com>
2021-06-06 17:58:34 +0530

    Updates

---
+++ precache-manifest.fa329a79c6ce9afe6b0d26762567e277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6820740ac1fbe1158674564e04717fe3",
+    "url": "/what-should-i-watch-next/index.html"
+  },
+  {
+    "revision": "9c02913fad084e40fe8e",
+    "url": "/what-should-i-watch-next/static/css/main.f51d4f78.chunk.css"
+  },
+  {
+    "revision": "449ef7cf7a421a2bae4b",
+    "url": "/what-should-i-watch-next/static/js/2.0e5e2e3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/what-should-i-watch-next/static/js/2.0e5e2e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c02913fad084e40fe8e",
+    "url": "/what-should-i-watch-next/static/js/main.e439fb6b.chunk.js"
+  },
+  {
+    "revision": "1e16141cff2d558f19a7",
+    "url": "/what-should-i-watch-next/static/js/runtime-main.b6851f3d.js"
+  },
+  {
+    "revision": "d54d3ecfbfb354f3b545deaf467c7c06",
+    "url": "/what-should-i-watch-next/static/media/LOGO1.d54d3ecf.png"
+  }
+]);
